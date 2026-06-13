86f5e6a3539f0937fff57ee793049d0eedf75b41
Gabriel <gabrielconceicao09@gmail.com>
2026-06-13 18:52:22 -0300

    timestamps sobreescrevidos com o tempo atual na chamada dos subscribers

--- src/mono-inertial/mono-inertial-node.cpp
+++ src/mono-inertial/mono-inertial-node.cpp
@@ -46,7 +46,7 @@ MonoInertialNode::~MonoInertialNode()
 void MonoInertialNode::GrabImu(const ImuMsg::SharedPtr msg)
 {
     RCLCPP_INFO(this->get_logger(), "GrabImu chamada");
-    //Não entendi muito bem, mas de alguma forma esse Mutex impede que outras threads alterem imuBuff_ ao mesmo tempo
+    msg->header.stamp = this->get_clock()->now();
     bufImuMutex_.lock();
     if (std::isnan(msg->linear_acceleration.x) || std::isnan(msg->linear_acceleration.y) || std::isnan(msg->linear_acceleration.z) ||
         std::isnan(msg->angular_velocity.x)    || std::isnan(msg->angular_velocity.y)    || std::isnan(msg->angular_velocity.z))
@@ -63,6 +63,7 @@ void MonoInertialNode::GrabImu(const ImuMsg::SharedPtr msg)
 void MonoInertialNode::GrabImage(const ImageMsg::SharedPtr msg)
 {
     RCLCPP_INFO(this->get_logger(), "GrabImage chamada");
+    msg->header.stamp = this->get_clock()->now();
     bufImgMutex_.lock();
     imgBuf_.push(msg);
     bufImgMutex_.unlock();
