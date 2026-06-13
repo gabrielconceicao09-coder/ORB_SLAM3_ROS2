fd6ea96850c43b683ee2d9a06551172039d4c4dd
Gabriel <gabrielconceicao09@gmail.com>
2026-06-13 17:21:52 -0300

    conserto nos nomes dos tópicos

--- src/mono-inertial/mono-inertial-node.cpp
+++ src/mono-inertial/mono-inertial-node.cpp
@@ -11,12 +11,12 @@ MonoInertialNode::MonoInertialNode(ORB_SLAM3::System* pSLAM)
     m_SLAM = pSLAM;
     // std::cout << "slam changed" << std::endl;
     m_image_subscriber = this->create_subscription<ImageMsg>(
-        "image_raw",
+        "/image_raw",
         rclcpp::SensorDataQoS(),
         std::bind(&MonoInertialNode::GrabImage, this, std::placeholders::_1));
     
     imu_subscriber = this->create_subscription<ImuMsg>(
-        "imu",
+        "/imu",
         rclcpp::SensorDataQoS(),
         std::bind(&MonoInertialNode::GrabImu, this, std::placeholders::_1));
 
