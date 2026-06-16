b5183b833e41e9e844c66f63e37aa5cfd90f92c3
Gabriel <gabrielconceicao09@gmail.com>
2026-06-16 18:01:55 -0300

    readicionar a publicação das transformadas com alguns ajustes e medidas de proteção

--- src/mono-inertial/mono-inertial-node.cpp
+++ src/mono-inertial/mono-inertial-node.cpp
@@ -67,7 +67,7 @@ void MonoInertialNode::GrabImage(const ImageMsg::SharedPtr msg)
     bufImgMutex_.lock();
     imgBuf_.push(msg);
     bufImgMutex_.unlock();
-    RCLCPP_INFO(this->get_logger(), "Mensagem camera recebida");
+    RCLCPP_INFO(this->get_logger(), "Mensagem câmera recebida");
 }
 
 cv::Mat MonoInertialNode::GetImage(const ImageMsg::SharedPtr msg)
@@ -87,118 +87,16 @@ cv::Mat MonoInertialNode::GetImage(const ImageMsg::SharedPtr msg)
 
     if (m_cvImPtr->image.type() == 0)
     {
+        RCLCPP_INFO(this->get_logger(), "GetImage rodou");
         return m_cvImPtr->image.clone();
     }
     else
     {
         RCLCPP_INFO(this->get_logger(), "Error image type");
+        RCLCPP_INFO(this->get_logger(), "GetImage rodou");
         return m_cvImPtr->image.clone();
     }
-    RCLCPP_INFO(this->get_logger(), "GetImage rodou");
 }
-
-/*void MonoInertialNode::SyncWithImu_Track()
-{   
-    
-    while(rclcpp::ok()) //Sempre rodando, i guess
-    {
-        RCLCPP_INFO(this->get_logger(), "Iteração de SyncWithImu_Track chamada");
-        cv::Mat Img;
-        
-        bufImgMutex_.lock();
-        if (imgBuf_.empty()){
-            bufImgMutex_.unlock();
-            std::this_thread::sleep_for(std::chrono::milliseconds(5));
-            continue;
-        }
-        bufImgMutex_.unlock();
-
-        bufImgMutex_.lock();
-        double tImg = Utility::StampToSec(imgBuf_.front()->header.stamp);
-        Img = GetImage(imgBuf_.front());
-        imgBuf_.pop();
-        bufImgMutex_.unlock();
-
-        vector<ORB_SLAM3::IMU::Point> vImuMeas;
-        bufImuMutex_.lock();
-
-        // CRÍTICO: Se o buffer da IMU estiver vazio OU a última mensagem do buffer 
-        // ainda for mais antiga que a imagem, precisamos esperar mais dados da IMU chegarem.
-        if (imuBuf_.empty() || Utility::StampToSec(imuBuf_.back()->header.stamp) < tImg)
-        {
-            bufImuMutex_.unlock();
-            std::this_thread::sleep_for(std::chrono::milliseconds(2));
-            bufImuMutex_.lock();
-            continue; 
-        }
-
-        if (!imuBuf_.empty())
-        {
-            //Load imu measurements from buffer
-            vImuMeas.clear();
-            while(!imuBuf_.empty() && Utility::StampToSec(imuBuf_.front()->header.stamp) <= tImg)
-            {
-                double t = Utility::StampToSec(imuBuf_.front()->header.stamp);
-                cv::Point3f acc(imuBuf_.front()->linear_acceleration.x, imuBuf_.front()->linear_acceleration.y, imuBuf_.front()->linear_acceleration.z);
-                cv::Point3f gyr(imuBuf_.front()->angular_velocity.x, imuBuf_.front()->angular_velocity.y, imuBuf_.front()->angular_velocity.z);
-                vImuMeas.push_back(ORB_SLAM3::IMU::Point(acc, gyr, t));
-                RCLCPP_INFO(this->get_logger(), "DEBUG SLAM -> Acc: [%f, %f, %f] | Gyr: [%f, %f, %f]", imuBuf_.front()->linear_acceleration.x, imuBuf_.front()->linear_acceleration.y, imuBuf_.front()->linear_acceleration.z, imuBuf_.front()->angular_velocity.x, imuBuf_.front()->angular_velocity.y, imuBuf_.front()->angular_velocity.z);
-                imuBuf_.pop();
-            }
-        }
-
-        // Adiciona a PRIMEIRA mensagem que é MAIOR que tImg (envelopamento necessário para o ORB-SLAM3)
-        // Mas NÃO damos pop() nela, pois ela serve de limite inicial para o próximo frame de imagem!
-        if(!imuBuf_.empty())
-        {
-            double t = Utility::StampToSec(imuBuf_.front()->header.stamp);
-            cv::Point3f acc(imuBuf_.front()->linear_acceleration.x, imuBuf_.front()->linear_acceleration.y, imuBuf_.front()->linear_acceleration.z);
-            cv::Point3f gyr(imuBuf_.front()->angular_velocity.x, imuBuf_.front()->angular_velocity.y, imuBuf_.front()->angular_velocity.z);
-            vImuMeas.push_back(ORB_SLAM3::IMU::Point(acc, gyr, t));
-        }
-
-        bufImuMutex_.unlock();
-
-        if(!vImuMeas.empty()) {
-            RCLCPP_INFO(this->get_logger(), "Passamos pela sincronização com IMU");
-            Sophus::SE3f Tcm = m_SLAM->TrackMonocular(Img, tImg, vImuMeas); //Tracking do orbslam3
-            RCLCPP_INFO(this->get_logger(), "TrackMonocular chamado");
-        }
-
-        /*Sophus::SE3f Tmc = Tcm.inverse(); //Transformação mapa => camera (está em base_link pela calibração do slam)
-        
-        TfMsg transf_msg;
-        try {
-            TfMsg odom_to_base_msg = tf_buffer_->lookupTransform("odom", "base_link", tf2::TimePointZero);
-            auto translation = odom_to_base_msg.transform.translation;
-            auto rotation = odom_to_base_msg.transform.rotation;
-            Eigen::Vector3f trans(translation.x, translation.y, translation.z);
-            Eigen::Quaternionf rot(rotation.w, rotation.x, rotation.y, rotation.z);
-            Sophus::SE3f Tob(rot, trans);
-            Sophus::SE3f Tbo = Tob.inverse();
-            Sophus::SE3f Tmo = Tmc * Tbo; //Transformação mapa => odometria, que o nav2 requisita
-
-            Eigen::Quaternionf Tmo_q(Tmo.rotationMatrix());
-            transf_msg.transform.translation.x = Tmo.translation().x();
-            transf_msg.transform.translation.x = Tmo.translation().y();
-            transf_msg.transform.translation.x = Tmo.translation().z();
-            transf_msg.transform.rotation.w = Tmo_q.w();
-            transf_msg.transform.rotation.x = Tmo_q.x();
-            transf_msg.transform.rotation.y = Tmo_q.y();
-            transf_msg.transform.rotation.z = Tmo_q.z();
-
-            transf_msg.header.stamp = this->get_clock()->now();
-            transf_msg.header.frame_id = "map";
-            transf_msg.child_frame_id = "odom";
-            tf_broadcaster_->sendTransform(transf_msg); //Publica transformação pelo tf2
-        } catch (const tf2::TransformException & ex) {
-            RCLCPP_INFO( this->get_logger(), "Could not find odom to base_link transform");
-            return;
-        }
-        //TODO: Talvez precise colocar um sleep igual o q tem em stereo-inertial.
-        /
-    }
-}*/
 
 //Solução do Gemini:
 
@@ -291,8 +189,127 @@ void MonoInertialNode::SyncWithImu_Track()
             }
 
             RCLCPP_INFO(this->get_logger(), "Passamos pela sincronização com IMU. Enviando %lu pontos.", vImuMeas.size());
+            try{
             Sophus::SE3f Tcm = m_SLAM->TrackMonocular(Img, tImg, vImuMeas);
             RCLCPP_INFO(this->get_logger(), "TrackMonocular chamado com sucesso!");
-        }
+            } catch (...) {
+                RCLCPP_INFO(this->get_logger(), "Algum problema com o tracking");
+                continue;
+            }
+            
+            if (m_SLAM->GetTrackingState() == ORBSLAM::Tracking::OK){
+
+                Sophus::SE3f Tmc = Tcm.inverse(); //Transformação mapa => camera 
+                TfMsg transf_msg;
+                try {
+                    TfMsg odom_to_base_msg = tf_buffer_->lookupTransform("odom", "base_link", tf2::TimePointZero);
+                    auto translation = odom_to_base_msg.transform.translation;
+                    auto rotation = odom_to_base_msg.transform.rotation;
+                    Eigen::Vector3f trans(translation.x, translation.y, translation.z);
+                    Eigen::Quaternionf rot(rotation.w, rotation.x, rotation.y, rotation.z);
+                    Sophus::SE3f Tob(rot, trans);
+                    Sophus::SE3f Tbo = Tob.inverse();
+                    Sophus::SE3f Tmo = Tmc * Tbo; //Transformação mapa => odometria, que o nav2 requisita (sem passar camera pra base_link)
+
+                    Eigen::Quaternionf Tmo_q(Tmo.rotationMatrix());
+                    transf_msg.transform.translation.x = Tmo.translation().x();
+                    transf_msg.transform.translation.y = Tmo.translation().y();
+                    transf_msg.transform.translation.z = Tmo.translation().z();
+                    transf_msg.transform.rotation.w = Tmo_q.w();
+                    transf_msg.transform.rotation.x = Tmo_q.x();
+                    transf_msg.transform.rotation.y = Tmo_q.y();
+                    transf_msg.transform.rotation.z = Tmo_q.z();
+
+                    transf_msg.header.stamp = this->get_clock()->now();
+                    transf_msg.header.frame_id = "map";
+                    transf_msg.child_frame_id = "odom";
+                    tf_broadcaster_->sendTransform(transf_msg); //Publica transformação pelo tf2
+                } catch (const tf2::TransformException & ex) {
+                    RCLCPP_INFO( this->get_logger(), "Could not find odom to base_link transform: %s", ex.what());
+                    continue;
+                }
+            } else {
+                RCLCPP_INFO(this->get_logger(), "Algo não está OK com o tracking: %d", m_SLAM->GetTrackingState());
+            }  
     }
 }
+
+/*void MonoInertialNode::SyncWithImu_Track()
+{   
+    
+    while(rclcpp::ok()) //Sempre rodando, enquanto o nó estiver rodando.
+    {
+        RCLCPP_INFO(this->get_logger(), "Iteração de SyncWithImu_Track chamada");
+        cv::Mat Img;
+        
+        bufImgMutex_.lock();
+        if (imgBuf_.empty()){
+            bufImgMutex_.unlock();
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+            continue;
+        }
+
+        double tImg = Utility::StampToSec(imgBuf_.front()->header.stamp);
+        Img = GetImage(imgBuf_.front());
+        imgBuf_.pop();
+        bufImgMutex_.unlock();
+
+        vector<ORB_SLAM3::IMU::Point> vImuMeas;
+        bufImuMutex_.lock();
+
+        if (!imuBuf_.empty())
+        {
+            // Load imu measurements from buffer
+            vImuMeas.clear();
+            while (!imuBuf_.empty() && Utility::StampToSec(imuBuf_.front()->header.stamp) <= tImg)
+            {
+                double t = Utility::StampToSec(imuBuf_.front()->header.stamp);
+                cv::Point3f acc(imuBuf_.front()->linear_acceleration.x, imuBuf_.front()->linear_acceleration.y, imuBuf_.front()->linear_acceleration.z);
+                cv::Point3f gyr(imuBuf_.front()->angular_velocity.x, imuBuf_.front()->angular_velocity.y, imuBuf_.front()->angular_velocity.z);
+                vImuMeas.push_back(ORB_SLAM3::IMU::Point(acc, gyr, t));
+                imuBuf_.pop();
+            }
+        }
+
+        bufImuMutex_.unlock();
+
+        if(!vImuMeas.empty()) {
+            RCLCPP_INFO(this->get_logger(), "Passamos pela sincronização com IMU");
+            Sophus::SE3f Tcm = m_SLAM->TrackMonocular(Img, tImg, vImuMeas); //Tracking do orbslam3
+            RCLCPP_INFO(this->get_logger(), "TrackMonocular chamado");
+        }
+
+        /*Sophus::SE3f Tmc = Tcm.inverse(); //Transformação mapa => camera (está em base_link pela calibração do slam)
+        
+        TfMsg transf_msg;
+        try {
+            TfMsg odom_to_base_msg = tf_buffer_->lookupTransform("odom", "base_link", tf2::TimePointZero);
+            auto translation = odom_to_base_msg.transform.translation;
+            auto rotation = odom_to_base_msg.transform.rotation;
+            Eigen::Vector3f trans(translation.x, translation.y, translation.z);
+            Eigen::Quaternionf rot(rotation.w, rotation.x, rotation.y, rotation.z);
+            Sophus::SE3f Tob(rot, trans);
+            Sophus::SE3f Tbo = Tob.inverse();
+            Sophus::SE3f Tmo = Tmc * Tbo; //Transformação mapa => odometria, que o nav2 requisita
+
+            Eigen::Quaternionf Tmo_q(Tmo.rotationMatrix());
+            transf_msg.transform.translation.x = Tmo.translation().x();
+            transf_msg.transform.translation.x = Tmo.translation().y();
+            transf_msg.transform.translation.x = Tmo.translation().z();
+            transf_msg.transform.rotation.w = Tmo_q.w();
+            transf_msg.transform.rotation.x = Tmo_q.x();
+            transf_msg.transform.rotation.y = Tmo_q.y();
+            transf_msg.transform.rotation.z = Tmo_q.z();
+
+            transf_msg.header.stamp = this->get_clock()->now();
+            transf_msg.header.frame_id = "map";
+            transf_msg.child_frame_id = "odom";
+            tf_broadcaster_->sendTransform(transf_msg); //Publica transformação pelo tf2
+        } catch (const tf2::TransformException & ex) {
+            RCLCPP_INFO( this->get_logger(), "Could not find odom to base_link transform");
+            return;
+        }
+        //TODO: Talvez precise colocar um sleep igual o q tem em stereo-inertial.
+        /
+    }
+}*/
