d8098f82c32dfae16b7a5c9f21f5bf15b22a41e4
Gabriel <gabrielconceicao09@gmail.com>
2026-06-27 23:09:05 -0300

    mensagem de debug da matriz q está sendo lida

--- src/mono-inertial/mono-inertial-node.cpp
+++ src/mono-inertial/mono-inertial-node.cpp
@@ -253,6 +253,12 @@ void MonoInertialNode::SyncWithImu_Track()
                         tImg, tPrimeiraImu, tUltimaImu, difTempos);
             
             try {
+                // PRINT DE DIAGNÓSTICO DA MATRIZ Tbc INTERNA DO SLAM
+                cv::Mat Tbc_orbslam = m_SLAM->GetTransformCameraToBody().matrix();
+                RCLCPP_INFO(this->get_logger(), "=== MATRIZ Tbc COMPILAÇÃO INTERNA ===");
+                RCLCPP_INFO(this->get_logger(), "[%f, %f, %f, %f]", Tbc_orbslam.at<float>(0,0), Tbc_orbslam.at<float>(0,1), Tbc_orbslam.at<float>(0,2), Tbc_orbslam.at<float>(0,3));
+                RCLCPP_INFO(this->get_logger(), "[%f, %f, %f, %f]", Tbc_orbslam.at<float>(1,0), Tbc_orbslam.at<float>(1,1), Tbc_orbslam.at<float>(1,2), Tbc_orbslam.at<float>(1,3));
+                RCLCPP_INFO(this->get_logger(), "[%f, %f, %f, %f]", Tbc_orbslam.at<float>(2,0), Tbc_orbslam.at<float>(2,1), Tbc_orbslam.at<float>(2,2), Tbc_orbslam.at<float>(2,3));
                 // Envia a imagem e o vetor contínuo blindado de IMU para o motor do SLAM
                 Sophus::SE3f Tcm = m_SLAM->TrackMonocular(Img, tImg, vImuMeas);
                 
